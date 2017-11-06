2ba51437371c9bccf1a0ad49d96ef957646e735c
Claudiu Vieru <satanel89@gmail.com>
2017-11-06 16:00:17 +0200

    added hero deteail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import {Component} from '@angular/core';
 
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   {id: 11, name: 'Mr. Nice'},
@@ -24,25 +20,15 @@ const HEROES: Hero[] = [
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-
     <h2>My Heroes</h2>
-
     <ul class="heroes">
-      <li [class.selected]="hero === selectedHero"
-          *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-
-
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <app-hero-detail [hero]="selectedHero"></app-hero-detail>
   `,
   styleUrls: ['./app.component.css']
 })
